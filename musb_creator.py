30d3d7950ef3a0006875e268a87093b2b06a8d1a
dumr666 <dumr666@gmail.com>
2018-04-26 18:28:45 +0200

    Initial commit

---
+++ musb_creator.py
@@ -0,0 +1,17 @@
+# Music USB creator for your autoradio
+# Usage:
+# python musb_creator.py <link to playlist> <destination drive>
+# must use m3u playlist
+
+#imports
+import sys
+# ID3 import
+from tinytag import TinyTag
+
+# Class for track
+class track():
+    def __init__(self, artist, title, path):
+        self.artist = artist
+        self.title = title
+        self.path = path
+
