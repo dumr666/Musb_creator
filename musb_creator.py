928e8457539984ac4e545dfd28c47483fbeaa34b
dumr666 <dumr666@gmail.com>
2018-04-29 23:52:34 +0200

    working version

--- musb_creator.py
+++ musb_creator.py
@@ -6,16 +6,19 @@
 
 #imports
 import sys
+import os
+import shutil
 # ID3 import
 from tinytag import TinyTag
 
 # Class for track
 class track():
-    def __init__(self, artist, title, path):
+    def __init__(self, artist, title, path, length):
         self.artist = artist
         self.title = title
+        self.length = length
         self.path = path
-
+        
 # M3U playlist parser
 def parsem3u(m3ufile):
     try:
@@ -33,40 +36,48 @@ def parsem3u(m3ufile):
         return
     # initialize playlist variables before reading file
     tracklist=[]
-    song=track(None,None,None)
+    song=track(None,None,None,None)
 
     for line in m3ufile:
         line=line.strip()
         if line.startswith('#EXTINF:'):
             # pull length and title from #EXTINF line
-            length,title=line.split('#EXTINF:')[1].split(',',1)
-            artist, song = title.split('-')
-            song=track(artist,song,None)
+            length,song_full=line.split('#EXTINF:')[1].split(',',1)
+            artist, title = song_full.split('-')
+            artist = artist.rstrip()
+            title = title.rstrip()
+            song=track(artist,title,None,length)
         elif (len(line) != 0):
             # pull song path from all other, non-blank lines
-            song.path=line
+            song_path = line.replace("/","\\")
+            song.path=song_path            
             tracklist.append(song)
-            # reset the song variable so it doesn't use the same EXTINF more than once
-            song=track(None,None,None)
+            song=track(None,None,None,None)
 
     m3ufile.close()
 
     return tracklist
+
 # Get script arguments 
 ScriptPath = str(sys.argv[0])
 PlaylistLink = str(sys.argv[1])
 DestDrive = str(sys.argv[2])
-# Do some magic with file paths
-PlaylistLink = PlaylistLink.replace("\\","\\\\")
-DestDrive = DestDrive.replace("\\", "\\\\")
 
-testline = "#EXTINF:261,Lamb Of God - Terminally Unique"
-length,artistline = testline.split('#EXTINF:')[1].split(',',1)
-print("dolzina tracka je: ", length)
-artist, song = artistline.split('-')
-print("artist: ", artist)
-print("song je: ", song)
 # Print out paths
 print("Script path: ", ScriptPath)
 print("Destination drive: ", DestDrive)
 print("Playlist link: ", PlaylistLink)
+tracklist = parsem3u(PlaylistLink)
+
+# Loop in tracklist
+for track in tracklist:
+    print(track.artist, track.title, track.length, " -- ", track.path)
+    usb_folder = DestDrive + track.artist
+    if not os.path.isdir(usb_folder):
+        print("Kreiranje mape na usbju: ", usb_folder)
+        os.makedirs(usb_folder)
+        print("Home directory %s was created." %usb_folder)
+    
+    # Copy mp3 to destination folder
+    print("kopiranje v mapo ", track.artist, track.title)
+    shutil.copy2(track.path, usb_folder)
